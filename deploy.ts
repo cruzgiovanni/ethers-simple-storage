b923f582dcecb821eb5c0a5a3caea567a8796676
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2025-09-03 15:16:03 -0300

    Update .gitignore, enhance deploy script, and add README documentation

--- deploy.ts
+++ deploy.ts
@@ -11,19 +11,43 @@ async function main() {
     )
 
     const abi = readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
-
     let binary = readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8")
 
     const factory = new ethers.ContractFactory(abi, binary, wallet)
-    const options = {
-      gasLimit: 3000000,
-    }
-    console.log("Deploying, please wait...")
-    const contract = await factory.deploy(options)
+    const contract = await factory.deploy({ gasLimit: 3000000 })
+    await contract.deploymentTransaction()?.wait(1)
 
-    console.log(contract)
+    const currentFavoriteNumber = await (contract as any).retrieve()
+    console.log(
+      `Current favorite number is: ${currentFavoriteNumber.toString()}`
+    )
 
-    process.exit(0)
+    const transactionResponse = await (contract as any).store(7, {
+      nonce: await wallet.getNonce(),
+    })
+    const transactionReceipt = await transactionResponse?.wait(1)
+    const updatedFavoriteNumber = await (contract as any).retrieve()
+    console.log(
+      `Updated favorite number is: ${updatedFavoriteNumber.toString()}`
+    )
+
+    // console.log("Let's deploy with only transaction data!")
+
+    // const tx = {
+    //   nonce: await wallet.getNonce(),
+    //   gasPrice: 20000000000,
+    //   gasLimit: 3000000,
+    //   to: null,
+    //   value: 0,
+    //   data: "0x" + binary,
+    //   chainId: 1337,
+    // }
+
+    // const sentTxResponse = await wallet.sendTransaction(tx) // under the hood it calls tx.sign() and tx.send()
+    // await sentTxResponse.wait(1)
+    // console.log(sentTxResponse)
+
+    // process.exit(0)
   } catch (error) {
     console.error(error)
     process.exit(1)
